3146e0e366303cfe3851e5242f5d5e4a07c1c2f5
Tahmid Tasin <shawontor@gmail.com>
2026-05-12 22:43:47 +0600

    first commit

---
+++ BookStore/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Big+Shoulders:opsz,wght@10..72,100..900&family=Bitcount+Prop+Double+Ink:wght@100..900&family=Boldonse&family=Changa+One:ital@0;1&family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Oswald:wght@200..700&family=Playwrite+IE:wght@100..400&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <title>Book Store</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav id="topbar">
+        <p id="logo">BookStore.com</p>
+        <input type="text" placeholder="Search your book" id="searchbook">
+        <button id="searchbtn"><ion-icon name="search-outline"></ion-icon></button>
+        <a href="" id="aboutus" class="aco">About Us</a>
+        <a href="" id="contactus" class="aco">Contact Us</a>
+        <a href="" id="location" class="aco">Outlet Location</a>
+        <img src="truck.png" id="truck">
+    </nav>
+</body>
+</html>
